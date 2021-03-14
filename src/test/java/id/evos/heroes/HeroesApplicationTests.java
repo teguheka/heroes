6555e0ca785adc64dd3eac8654570d61ad70f41a
Teguh Eka Putra <teguh.putra@dana.id>
2021-03-14 19:52:35 +0700

    commit init project

---
+++ src/test/java/id/evos/heroes/HeroesApplicationTests.java
@@ -0,0 +1,13 @@
+package id.evos.heroes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeroesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
